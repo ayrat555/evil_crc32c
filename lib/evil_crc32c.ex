7b7eb649a24f62ccb7b877db21ac170c075b77a5
Ayrat Badykov <ayratin555@gmail.com>
2022-12-12 13:32:21 +0200

    add elixir code

--- lib/evil_crc32c.ex
+++ lib/evil_crc32c.ex
@@ -1,18 +1,72 @@
 defmodule EvilCrc32c do
-  @moduledoc """
-  Documentation for `EvilCrc32c`.
-  """
+  alias EvilCrc32c.Impl
 
   @doc """
-  Hello world.
+  Calculate the CRC.
 
-  ## Examples
+  Expect binary data.
 
-      iex> EvilCrc32c.hello()
-      :world
+  #### Examples
 
+      iex> EvilCrc32c.calc(<<1, 2, 3, 4>>)
+      {:ok, <<244, 140, 48, 41>>}
+
+      iex> EvilCrc32c.calc(5)
+      {:error, :not_binary_data}
+
+      iex> EvilCrc32c.calc("hello", false)
+      {:ok, -1703822516}
+
+      iex> EvilCrc32c.calc("hello", true)
+      {:ok, <<76, 187, 113, 154>>}
   """
-  def hello do
-    :world
+  @spec calc(binary(), boolean()) :: {:ok, integer() | binary()} | {:error, :not_binary_data}
+  def calc(data, binary \\ true)
+
+  def calc(data, binary) when is_binary(data) do
+    result = do_calc(data, binary)
+
+    {:ok, result}
+  end
+
+  def calc(_data, _flag) do
+    {:error, :not_binary_data}
+  end
+
+  @doc """
+  Calculate the CRC. Raises an Argument error if data is not binary
+
+  Expect binary data.
+
+  #### Examples
+
+      iex> EvilCrc32c.calc!(<<1, 2, 3, 4>>)
+      <<244, 140, 48, 41>>
+
+      iex> EvilCrc32c.calc!("hello", false)
+      -1703822516
+
+      iex> EvilCrc32c.calc!("hello", true)
+      <<76, 187, 113, 154>>
+  """
+  @spec calc(binary(), boolean()) :: non_neg_integer() | no_return()
+  def calc!(data, binary \\ true)
+
+  def calc!(data, binary) do
+    do_calc(data, binary)
+  end
+
+  defp do_calc(data, binary) do
+    number = Impl.calc(data)
+
+    if binary do
+      to_binary(number)
+    else
+      number
+    end
+  end
+
+  defp to_binary(integer) do
+    <<integer::32-little>>
   end
 end
