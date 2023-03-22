79d2a703165522f6761226a2366952e670f5c4ca
Ayrat Badykov <ayratin555@gmail.com>
2023-03-22 11:20:20 +0200

    Implement CRC16 (#9)
    * Implement CRC16
    
    * fix typo

--- lib/evil_crc32c.ex
+++ lib/evil_crc32c.ex
@@ -1,67 +1,81 @@
 defmodule EvilCrc32c do
   @moduledoc """
-  "Evil" version of the crc32c algorithm. It uses the bitwise arithmetic used in javascript,
+  "Evil" versions of the crc32c and crc16 algorithm. It uses the bitwise arithmetic used in javascript,
   i.e. operands are converted to i32
   """
   alias EvilCrc32c.Impl
 
   @doc """
-  Calculate the CRC.
-
-  Expect binary data.
+  Calculate the CRC32C.
 
   #### Examples
 
-      iex> EvilCrc32c.calc(<<1, 2, 3, 4>>)
+      iex> EvilCrc32c.crc32c(<<1, 2, 3, 4>>)
       {:ok, <<244, 140, 48, 41>>}
 
-      iex> EvilCrc32c.calc(5)
+      iex> EvilCrc32c.crc32c(5)
       {:error, :not_binary_data}
 
-      iex> EvilCrc32c.calc("hello", false)
+      iex> EvilCrc32c.crc32c("hello", false)
       {:ok, -1703822516}
 
-      iex> EvilCrc32c.calc("hello", true)
+      iex> EvilCrc32c.crc32c("hello", true)
       {:ok, <<76, 187, 113, 154>>}
   """
-  @spec calc(binary(), boolean()) :: {:ok, integer() | binary()} | {:error, :not_binary_data}
-  def calc(data, binary \\ true)
+  @spec crc32c(binary(), boolean()) :: {:ok, integer() | binary()} | {:error, :not_binary_data}
+  def crc32c(data, binary \\ true)
 
-  def calc(data, binary) when is_binary(data) do
-    result = do_calc(data, binary)
+  def crc32c(data, binary) when is_binary(data) do
+    result = do_crc32c(data, binary)
 
     {:ok, result}
   end
 
-  def calc(_data, _flag) do
+  def crc32c(_data, _flag) do
     {:error, :not_binary_data}
   end
 
   @doc """
-  Calculate the CRC. Raises an Argument error if data is not binary
-
-  Expect binary data.
+  Calculate the CRC32C. Raises an Argument error if data is not binary
 
   #### Examples
 
-      iex> EvilCrc32c.calc!(<<1, 2, 3, 4>>)
+      iex> EvilCrc32c.crc32c!(<<1, 2, 3, 4>>)
       <<244, 140, 48, 41>>
 
-      iex> EvilCrc32c.calc!("hello", false)
+      iex> EvilCrc32c.crc32c!("hello", false)
       -1703822516
 
-      iex> EvilCrc32c.calc!("hello", true)
+      iex> EvilCrc32c.crc32c!("hello", true)
       <<76, 187, 113, 154>>
   """
-  @spec calc!(binary(), boolean()) :: integer() | binary() | no_return()
-  def calc!(data, binary \\ true)
+  @spec crc32c!(binary(), boolean()) :: integer() | binary() | no_return()
+  def crc32c!(data, binary \\ true)
 
-  def calc!(data, binary) do
-    do_calc(data, binary)
+  def crc32c!(data, binary) do
+    do_crc32c(data, binary)
   end
 
-  defp do_calc(data, binary) do
-    number = Impl.calc(data)
+  @doc """
+  Calculate the CRC16.
+
+  #### Examples
+
+      iex> EvilCrc32c.crc16("abcdef")
+      <<58, 253>>
+  """
+  @spec crc16(binary()) :: binary()
+  def crc16(data) do
+    reg = Impl.calc_crc16(data)
+
+    remainder = rem(reg, 256)
+    floor_div = Float.floor(reg / 256.0) |> trunc()
+
+    <<floor_div, remainder>>
+  end
+
+  defp do_crc32c(data, binary) do
+    number = Impl.calc_crc32c(data)
 
     if binary do
       to_binary(number)
--- test/evil_crc32c_test.exs
+++ test/evil_crc32c_test.exs
@@ -3,37 +3,45 @@ defmodule EvilCrc32cTest do
 
   doctest EvilCrc32c
 
-  describe "calc/1" do
+  describe "crc32c/1" do
     test "calculates crc32c" do
       {:ok, source} =
         Base.decode64(
           "te6ccgECFAEAAtQAART/APSkE/S88sgLAQIBIAIDAgFIBAUE+PKDCNcYINMf0x/THwL4I7vyZO1E0NMf0x/T//QE0VFDuvKhUVG68qIF+QFUEGT5EPKj+AAkpMjLH1JAyx9SMMv/UhD0AMntVPgPAdMHIcAAn2xRkyDXSpbTB9QC+wDoMOAhwAHjACHAAuMAAcADkTDjDQOkyMsfEssfy/8QERITAubQAdDTAyFxsJJfBOAi10nBIJJfBOAC0x8hghBwbHVnvSKCEGRzdHK9sJJfBeAD+kAwIPpEAcjKB8v/ydDtRNCBAUDXIfQEMFyBAQj0Cm+hMbOSXwfgBdM/yCWCEHBsdWe6kjgw4w0DghBkc3RyupJfBuMNBgcCASAICQB4AfoA9AQw+CdvIjBQCqEhvvLgUIIQcGx1Z4MesXCAGFAEywUmzxZY+gIZ9ADLaRfLH1Jgyz8gyYBA+wAGAIpQBIEBCPRZMO1E0IEBQNcgyAHPFvQAye1UAXKwjiOCEGRzdHKDHrFwgBhQBcsFUAPPFiP6AhPLassfyz/JgED7AJJfA+ICASAKCwBZvSQrb2omhAgKBrkPoCGEcNQICEekk30pkQzmkD6f+YN4EoAbeBAUiYcVnzGEAgFYDA0AEbjJftRNDXCx+AA9sp37UTQgQFA1yH0BDACyMoHy//J0AGBAQj0Cm+hMYAIBIA4PABmtznaiaEAga5Drhf/AABmvHfaiaEAQa5DrhY/AAG7SB/oA1NQi+QAFyMoHFcv/ydB3dIAYyMsFywIizxZQBfoCFMtrEszMyXP7AMhAFIEBCPRR8qcCAHCBAQjXGPoA0z/IVCBHgQEI9FHyp4IQbm90ZXB0gBjIywXLAlAGzxZQBPoCFMtqEssfyz/Jc/sAAgBsgQEI1xj6ANM/MFIkgQEI9Fnyp4IQZHN0cnB0gBjIywXLAlAFzxZQA/oCE8tqyx8Syz/Jc/sAAAr0AMntVA=="
         )
 
-      assert {:ok, 1_580_765_482} = EvilCrc32c.calc(source, false)
-      assert {:ok, <<42, 145, 56, 94>>} = EvilCrc32c.calc(source)
+      assert {:ok, 1_580_765_482} = EvilCrc32c.crc32c(source, false)
+      assert {:ok, <<42, 145, 56, 94>>} = EvilCrc32c.crc32c(source)
     end
 
     test "fails to calculate crc32c for non-binary data" do
-      assert {:error, :not_binary_data} = EvilCrc32c.calc(%{})
+      assert {:error, :not_binary_data} = EvilCrc32c.crc32c(%{})
     end
   end
 
-  describe "calc!/1" do
+  describe "crc32c!/1" do
     test "calculates crc32c" do
       {:ok, source} =
         Base.decode64(
           "te6ccgECFAEAAtQAART/APSkE/S88sgLAQIBIAIDAgFIBAUE+PKDCNcYINMf0x/THwL4I7vyZO1E0NMf0x/T//QE0VFDuvKhUVG68qIF+QFUEGT5EPKj+AAkpMjLH1JAyx9SMMv/UhD0AMntVPgPAdMHIcAAn2xRkyDXSpbTB9QC+wDoMOAhwAHjACHAAuMAAcADkTDjDQOkyMsfEssfy/8QERITAubQAdDTAyFxsJJfBOAi10nBIJJfBOAC0x8hghBwbHVnvSKCEGRzdHK9sJJfBeAD+kAwIPpEAcjKB8v/ydDtRNCBAUDXIfQEMFyBAQj0Cm+hMbOSXwfgBdM/yCWCEHBsdWe6kjgw4w0DghBkc3RyupJfBuMNBgcCASAICQB4AfoA9AQw+CdvIjBQCqEhvvLgUIIQcGx1Z4MesXCAGFAEywUmzxZY+gIZ9ADLaRfLH1Jgyz8gyYBA+wAGAIpQBIEBCPRZMO1E0IEBQNcgyAHPFvQAye1UAXKwjiOCEGRzdHKDHrFwgBhQBcsFUAPPFiP6AhPLassfyz/JgED7AJJfA+ICASAKCwBZvSQrb2omhAgKBrkPoCGEcNQICEekk30pkQzmkD6f+YN4EoAbeBAUiYcVnzGEAgFYDA0AEbjJftRNDXCx+AA9sp37UTQgQFA1yH0BDACyMoHy//J0AGBAQj0Cm+hMYAIBIA4PABmtznaiaEAga5Drhf/AABmvHfaiaEAQa5DrhY/AAG7SB/oA1NQi+QAFyMoHFcv/ydB3dIAYyMsFywIizxZQBfoCFMtrEszMyXP7AMhAFIEBCPRR8qcCAHCBAQjXGPoA0z/IVCBHgQEI9FHyp4IQbm90ZXB0gBjIywXLAlAGzxZQBPoCFMtqEssfyz/Jc/sAAgBsgQEI1xj6ANM/MFIkgQEI9Fnyp4IQZHN0cnB0gBjIywXLAlAFzxZQA/oCE8tqyx8Syz/Jc/sAAAr0AMntVA=="
         )
 
-      assert 1_580_765_482 = EvilCrc32c.calc!(source, false)
-      assert <<42, 145, 56, 94>> = EvilCrc32c.calc!(source)
+      assert 1_580_765_482 = EvilCrc32c.crc32c!(source, false)
+      assert <<42, 145, 56, 94>> = EvilCrc32c.crc32c!(source)
     end
 
     test "fails to calculate crc32c for non-binary data" do
       assert_raise ArgumentError, "argument error", fn ->
-        EvilCrc32c.calc!(%{})
+        EvilCrc32c.crc32c!(%{})
       end
+    end
+  end
+
+  describe "crc16/1" do
+    test "calculate crc16" do
+      data = "hello"
+
+      assert <<195, 98>> == EvilCrc32c.crc16(data)
     end
   end
 end
